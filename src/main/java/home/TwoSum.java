b3b3f0bb6eed624e50cc063fc951a792108b5429
duxf <ontway@163.com>
2017-10-22 15:18:51 +0800

    acer 1

--- src/main/java/home/TwoSum.java
+++ src/main/java/home/TwoSum.java
@@ -4,60 +4,6 @@ import com.sun.deploy.uitoolkit.impl.awt.AWTAppletAdapter;
 
 public class TwoSum {
 	public static void main(String[] args) {
-		aaa ss dd
-		aaa
-		aa
-		aa
-		bbb
-		ccc
-		int a=1;
-		aaa
-		ddd
-		ddd
-		eee
-		ddd
-		int a=1;
-		eee
-		int b=2;
-		ddd
-		int c=3;
-		int e=5;
-		int d=4;
-		bbb
-		System.out.println(a);
-		int f=2;
-		System.out.println(b);
-		int e=7;
-		ccc
-		ddd
-		System.out.println(c);
-		int g=4;
+          
 	}
-
-	public int[] twoSum(int[] nums, int target) {
-        int i1=0,j1=0;
-
-		for(int i=0;i<nums.length;i++){
-			int a=nums[i];
-			int b=target-nums[i];
-		    for(int j=i+1;j<nums.length;j++){
-		        j1=j;
-		    	if(b==nums[j]){
-		    	
-		    		break;
-		    	}
-		    }
-		    if(nums[j1]+a==target){
-		    	i1=i;
-		        break;}
-		    
-		    }
-
-		 int[] twonums={i1,j1};
-		 return twonums;}
-		 int aa=1;
-		 int bb=2;
-		 int cc=3;
-
-		 
-    }
+}
