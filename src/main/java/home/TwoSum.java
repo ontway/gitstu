9ee5277863f4a841893d2001a09042646ebbd821
duxf <ontway@163.com>
2017-10-22 15:20:27 +0800

    acer r1 1

--- src/main/java/home/TwoSum.java
+++ src/main/java/home/TwoSum.java
@@ -1,31 +1,7 @@
 package home;
 
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        int i1=0,j1=0;
-
-		for(int i=0;i<nums.length;i++){
-			int a=nums[i];
-			int b=target-nums[i];
-		    for(int j=i+1;j<nums.length;j++){
-		        j1=j;
-		    	if(b==nums[j]){
-		    	
-		    		break;
-		    	}
-		    }
-		    if(nums[j1]+a==target){
-		    	i1=i;
-		        break;}
-		    
-		    }
-		
-		// if(nums[j1]+nums[i1]==target){
-		 int[] twonums={i1,j1};
-		 return twonums;}
-		// else {
-		//	 System.out.println("error");
-		//	 int[] twonums={0,0};
-		//	 return twonums;
-		 
-    }
+	public int[] twoSum(int[] nums, int target) {
+            return null;
+	}
+}
